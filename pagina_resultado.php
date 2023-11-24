cdc9f66cbe29116609e5f556bd3543115d90acdb
Estevan Santiago Dutra <87251849+EstevanSantiagoDutra@users.noreply.github.com>
2023-11-24 00:26:57 -0300

    Update pagina_resultado.php

--- pagina_resultado.php
+++ pagina_resultado.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultados do Churrasco</title>
+    <title>Estevan</title>
 </head>
 
 <body>
-    <h1>Resultados do Churrasco</h1>
+    <h1>Churrasco EstevanS</h1>
 
     <?php
     $dbHost = "localhost";
